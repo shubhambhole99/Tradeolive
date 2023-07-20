639059027225ee72340801124a05c8250c923cac
shubhambhole99 <shubhambhole68@gmail.com>
2023-07-21 04:03:55 +0530

    initial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,14 @@ export const Home=()=>{
             <p>Learn Trading is a manner which you have never learned before<br/>with proper Risk management Techniques</p>
             </div>
         </div>
+        <div id='author'>
+            <h1>priyanshoo shirsat</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia recusandae, iste, qui commodi sunt a, eaque consequuntur quibusdam perspiciatis odio deleniti dolor. Tempora maxime expedita neque dolor ea iure quod.</p>
+        </div>
+        <div id='social-media'>
+            <h1>Find Us on</h1>
+            <p>priyanshoo shirsat has a strong community of 1000+ followers across all social media platforms</p>
+        </div>
         </>
        )
 }
