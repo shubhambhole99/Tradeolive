3170c9cbb8072cab5d1ee1421c82c7353c965fec
shubhambhole99 <shubhambhole68@gmail.com>
2023-07-21 22:40:33 +0530

    f

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
-import {Home} from './pages/home'
+import {Home} from './pages/Home'
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import {Carousel} from './Components/Carousel'
 
