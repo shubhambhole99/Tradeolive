bd5d0edcb023c9e80c8ef6ae3b457950579c6ff3
shubhambhole99 <shubhambhole68@gmail.com>
2023-07-31 18:16:39 +0530

    f

--- src/pages/BookNow.jsx
+++ src/pages/BookNow.jsx
@@ -2,42 +2,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { useState, useEffect } from "react";
 import React, { useRef } from 'react';
 import {Navbar} from '../Components/Navbar'
-import {Carousel} from '../Components/Carousel'
 import {Footer} from '../Components/Footer'
-import { Container } from "../Components/Container";
-import {Link} from 'react-router-dom';
-import students from '../assets/students.png'
-import pt from '../assets/personal-trainer.png'
-import complete from '../assets/completed-task.png'
-import courses from '../assets/courses.png'
+import call from '../assets/call.png'
+import bookus from '../assets/appointment.png'
+import email from '../assets/email.png'
+
 import '../styles/booknow.css'
 
-const data1=[
-    {   id:1,
-        name:"10K+",
-        description:"Online Learners",
-        img:students
-    },
-    {   id:2,
-        name:"25K+",
-        description:"Satisfied Students",
-        img:complete
-    },
-    {
-        id:4,
-        name:"20+",
-        description:"Instructors",
-        img:pt
-    },
-    {
-        id:5,
-        name:"10+",
-        description:"Courses",
-        img:courses
-    }
-   
-   
-]
+
 
 export const BookNow=()=>{
     return (<>
@@ -52,30 +24,25 @@ export const BookNow=()=>{
    </div>
     
     </div>
-    <div id='contact'>
+    <div id='contact-book'>
         <div>
             <h1>Contact Us</h1>
             {/* Call us */}
             {/* Email us */}
             {/* Book and Appointment */}
-            <p>Level Up Your Trading Game With Our Daily Content</p>
+            {/* <p>Level Up Your Trading Game With Our Daily Content</p> */}
             <br/>
             
 
-{/* <div id='icons-contact'>
-    <img onClick={()=>{window.open('https://www.youtube.com/', '_blank')}} id='Youtube' src={youtube}></img>
-    <img id='Instagram' src={instagram}></img>
-
+<div id='icons-book'>
+    <img onClick={()=>{window.open('https://www.youtube.com/', '_blank')}} id='Youtube' src={call}></img>
+    <img id='Instagram' src={email}></img>    
+    <img id='Twitter' src={bookus}></img>
+    <h2>Call Us</h2>
+    <h2>Email</h2>
+    <h2>Book an Appointment</h2>
     
-    <img id='Twitter' src={twitter}></img>
-    <img id='LinkedIn' src={linkedin}></img>
-    <img id='Facebook' src={facebook}></img>
-    <h2>Youtube</h2>
-    <h2>Instagram</h2>
-    <h2>Twitter</h2>
-    <h2>LinkedIn</h2>
-    <h2>Facebook</h2>
-</div> */}
+</div>
           
             <br/>
         </div>
